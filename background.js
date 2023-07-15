b96d596b7d3b3d0fce7150b631a3b37c755b9c01
devacadian <135052510+devacadian@users.noreply.github.com>
2023-07-15 08:26:57 -0300

    working counter, working timer (double counts & currently isnt rolling or dynamic count)

--- background.js
+++ background.js
@@ -1,24 +1,32 @@
-let count = 0;
-let startTime = null;
 let timer = null;
+
+function startTimer() {
+    let startTime = Date.now();
+    timer = setInterval(function() {
+        let elapsedTime = Math.floor((Date.now() - startTime) / 1000);
+        let remainingTime = Math.max(0, 3*60*60 - elapsedTime);
+        chrome.storage.local.set({'time': remainingTime}, function() {
+            if (remainingTime === 0) {
+                clearInterval(timer);
+                timer = null;
+                chrome.storage.local.set({'count': 0});
+            }
+        });
+    }, 1000);
+}
 
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
         if (details.method === 'POST' && details.url.includes('https://chat.openai.com/')) {
-            count++;
-            if (count === 1) {
-                startTime = Date.now();
-                timer = setInterval(function() {
-                    let elapsedTime = Date.now() - startTime;
-                    chrome.storage.local.set({'time': Math.max(0, 3*60*60 - Math.floor(elapsedTime / 1000))}, function() {
-                        if (elapsedTime >= 3 * 60 * 60 * 1000) {
-                            clearInterval(timer);
-                            timer = null;
-                        }
-                    });
-                }, 1000);
-            }
-            chrome.storage.local.set({'count': count});
+            chrome.storage.local.get(['count'], function(result) {
+                let count = result.count || 0;
+                count++;
+                if (count === 1) {
+                    if (timer) clearInterval(timer);
+                    startTimer();
+                }
+                chrome.storage.local.set({'count': count});
+            });
         }
     },
     {urls: ["<all_urls>"]}
@@ -26,19 +34,9 @@ chrome.webRequest.onBeforeRequest.addListener(
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.reset) {
-        count = 0;
-        startTime = Date.now();
-        chrome.storage.local.set({'count': count, 'time': 3*60*60}, function() {
-            if (timer) clearInterval(timer);
-            timer = setInterval(function() {
-                let elapsedTime = Date.now() - startTime;
-                chrome.storage.local.set({'time': Math.max(0, 3*60*60 - Math.floor(elapsedTime / 1000))}, function() {
-                    if (elapsedTime >= 3 * 60 * 60 * 1000) {
-                        clearInterval(timer);
-                        timer = null;
-                    }
-                });
-            }, 1000);
+        if (timer) clearInterval(timer);
+        chrome.storage.local.set({'count': 0, 'time': 3*60*60}, function() {
+            startTimer();
         });
     }
 });
--- popup.js
+++ popup.js
@@ -1,23 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
+    updateDisplay();
+
+    document.getElementById('reset').addEventListener('click', function() {
+        chrome.storage.local.set({'count': 0, 'time': 3*60*60}, function() {
+            chrome.runtime.sendMessage({reset: true});
+            updateDisplay();
+        });
+    });
+
+    setInterval(updateDisplay, 1000);
+});
+
+function updateDisplay() {
     chrome.storage.local.get(['count', 'time'], function(result) {
         document.getElementById('count').textContent = result.count || '0';
         document.getElementById('time').textContent = formatTime(result.time || 3*60*60);
     });
-
-    document.getElementById('reset').addEventListener('click', function() {
-        chrome.storage.local.set({'count': 0, 'time': 3*60*60}, function() {
-            document.getElementById('count').textContent = '0';
-            document.getElementById('time').textContent = formatTime(3*60*60);
-        });
-        chrome.runtime.sendMessage({reset: true});
-    });
-
-    setInterval(function() {
-        chrome.storage.local.get(['time'], function(result) {
-            document.getElementById('time').textContent = formatTime(result.time || 0);
-        });
-    }, 1000);
-});
+}
 
 function formatTime(seconds) {
     let hours = Math.floor(seconds / 3600);
