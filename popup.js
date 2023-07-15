45927013f41aee81bc3c16afbaa4f68bd757f7de
devacadian <135052510+devacadian@users.noreply.github.com>
2023-07-15 07:58:59 -0300

    prompt counter working - reset working

--- popup.js
+++ popup.js
@@ -1,6 +1,11 @@
-
 document.addEventListener('DOMContentLoaded', function() {
     chrome.storage.local.get(['count'], function(result) {
         document.getElementById('count').textContent = result.count || '0';
+    });
+
+    document.getElementById('reset').addEventListener('click', function() {
+        chrome.storage.local.set({'count': 0}, function() {
+            document.getElementById('count').textContent = '0';
+        });
     });
 });
