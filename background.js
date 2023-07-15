e121f956f0a912337a91fa9cf0e3950b171b8a88
devacadian <135052510+devacadian@users.noreply.github.com>
2023-07-15 08:02:53 -0300

    prompt count not actually getting reset - bug - time not counting down either

--- background.js
+++ background.js
@@ -1,22 +1,44 @@
-
 let count = 0;
-let startTime = Date.now();
+let startTime = null;
+let timer = null;
 
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
         if (details.method === 'POST' && details.url.includes('https://chat.openai.com/')) {
             count++;
+            if (count === 1) {
+                startTime = Date.now();
+                timer = setInterval(function() {
+                    let elapsedTime = Date.now() - startTime;
+                    chrome.storage.local.set({'time': Math.max(0, 3*60*60 - Math.floor(elapsedTime / 1000))}, function() {
+                        if (elapsedTime >= 3 * 60 * 60 * 1000) {
+                            clearInterval(timer);
+                            timer = null;
+                        }
+                    });
+                }, 1000);
+            }
             chrome.storage.local.set({'count': count});
         }
     },
     {urls: ["<all_urls>"]}
 );
 
-setInterval(function() {
-    let elapsedTime = Date.now() - startTime;
-    if (elapsedTime >= 3 * 60 * 60 * 1000) {
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.reset) {
         count = 0;
         startTime = Date.now();
-        chrome.storage.local.set({'count': count});
+        chrome.storage.local.set({'count': count, 'time': 3*60*60}, function() {
+            if (timer) clearInterval(timer);
+            timer = setInterval(function() {
+                let elapsedTime = Date.now() - startTime;
+                chrome.storage.local.set({'time': Math.max(0, 3*60*60 - Math.floor(elapsedTime / 1000))}, function() {
+                    if (elapsedTime >= 3 * 60 * 60 * 1000) {
+                        clearInterval(timer);
+                        timer = null;
+                    }
+                });
+            }, 1000);
+        });
     }
-}, 1000);
+});
--- popup.js
+++ popup.js
@@ -1,11 +1,27 @@
 document.addEventListener('DOMContentLoaded', function() {
-    chrome.storage.local.get(['count'], function(result) {
+    chrome.storage.local.get(['count', 'time'], function(result) {
         document.getElementById('count').textContent = result.count || '0';
+        document.getElementById('time').textContent = formatTime(result.time || 3*60*60);
     });
 
     document.getElementById('reset').addEventListener('click', function() {
-        chrome.storage.local.set({'count': 0}, function() {
+        chrome.storage.local.set({'count': 0, 'time': 3*60*60}, function() {
             document.getElementById('count').textContent = '0';
+            document.getElementById('time').textContent = formatTime(3*60*60);
         });
+        chrome.runtime.sendMessage({reset: true});
     });
+
+    setInterval(function() {
+        chrome.storage.local.get(['time'], function(result) {
+            document.getElementById('time').textContent = formatTime(result.time || 0);
+        });
+    }, 1000);
 });
+
+function formatTime(seconds) {
+    let hours = Math.floor(seconds / 3600);
+    let minutes = Math.floor((seconds % 3600) / 60);
+    seconds = seconds % 60;
+    return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+}
